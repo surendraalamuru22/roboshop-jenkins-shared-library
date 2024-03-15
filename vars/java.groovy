ddd77f8b898c77bbc8d888b599e586cd064e01b3
surendra <surendraredddyalamuru01@gmail.com>
2024-03-15 10:19:20 +0530

    code quality

--- vars/java.groovy
+++ vars/java.groovy
@@ -1,59 +1,55 @@
 def call() {
-    pipeline {
+    node {
         agent {
             node { label 'workstation'}
         }
-        stages {
-            stage ('compile code') {
-                steps {
-                    echo 'compile code'
-                }
-            }
-            stage ('code quality') {
-                steps {
-                    echo 'code quality'
-                }
-            }
-            stage ('lint checks') {
-                when {
-                    anyOf {
-                        branch 'main'
-                        tag "*"
-                    }
-                }
-                steps {
-                    echo 'lint checks'
-                }
-            }
-            stage ('unit tests') {
-                when {
-                    anyOf {
-                        branch 'main'
-                        tag "*"
-                    }
-                }
-                steps {
-                    echo 'unit tests'
-                }
-            }
-            stage ('Build package') {
-                when { tag "*" }
-                steps {
-                    echo 'Build package'
-                }
-            }
-            stage ('prepare artifact') {
-                when { tag "*" }
-                steps {
-                    echo 'prepare artifact'
-                }
-            }
-            stage ('publish artifact') {
-                when { tag "*" }
-                steps {
-                    echo 'publish artifact'
-                }
-            }
-        }
+//        stages {
+//            stage ('compile code') {
+//                steps {
+//                    echo 'compile code'
+//                }
+//            }
+            common.codeQuality()
+//            stage ('lint checks') {
+//                when {
+//                    anyOf {
+//                        branch 'main'
+//                        tag "*"
+//                    }
+//                }
+//                steps {
+//                    echo 'lint checks'
+//                }
+//            }
+//            stage ('unit tests') {
+//                when {
+//                    anyOf {
+//                        branch 'main'
+//                        tag "*"
+//                    }
+//                }
+//                steps {
+//                    echo 'unit tests'
+//                }
+//            }
+//            stage ('Build package') {
+//                when { tag "*" }
+//                steps {
+//                    echo 'Build package'
+//                }
+//            }
+//            stage ('prepare artifact') {
+//                when { tag "*" }
+//                steps {
+//                    echo 'prepare artifact'
+//                }
+//            }
+//            stage ('publish artifact') {
+//                when { tag "*" }
+//                steps {
+//                    echo 'publish artifact'
+//                }
+//            }
+//        }
     }
 }
