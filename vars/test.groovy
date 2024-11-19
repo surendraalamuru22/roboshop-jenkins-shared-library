f8c5eabff6530c35be049f2dfd490e4a0e947e0f
surendra <surendraredddyalamuru01@gmail.com>
2024-11-19 15:53:13 +0530

    env variables

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,14 +1,23 @@
-def call() {
-    env.PIPELINE_NAME = "surendra"
-   pipeline {
-       agent any
+//def call() {
+//    env.PIPELINE_NAME = "surendra"
+//   pipeline {
+//       agent any
+//
+//       stages {
+//           stage('test stage') {
+//               steps {
+//                   sh 'echo name of the pipeline is $PIPELINE_NAME'
+//               }
+//           }
+//       }
+//   }
+//}
 
-       stages {
-           stage('test stage') {
-               steps {
-                   sh 'echo name of the pipeline is $PIPELINE_NAME'
-               }
-           }
-       }
-   }
+def call() {
+    node {
+        agent any
+        stage('test') {
+            sh 'echo hello world'
+        }
+    }
 }
