ae4e1d68ecf76a13615cb68c4c89d581dfc0ad4f
surendra <surendraredddyalamuru01@gmail.com>
2024-11-19 15:09:00 +0530

    env variables

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,10 +1,11 @@
 def call() {
    pipeline {
        agent any
+       env.PIPELINE_NAME = "surendra"
        stages {
            stage('test stage') {
                steps {
-                   sh 'echo hello world'
+                   sh 'echo name of the pipeline is $PIPE_LINE'
                }
            }
        }
